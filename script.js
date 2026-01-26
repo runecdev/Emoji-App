a92b98ff0cd0e6981e5f0b5c09f8d09bf820952d
runecdev <runec.dev@gmail.com>
2026-01-26 17:42:38 +0530

    History added

--- script.js
+++ script.js
@@ -1,43 +1,96 @@
 // Variables
 const url =
     "https://emoji-api.com/emojis?access_key=8c1a1c5ffa52d6836012e7a184e9e9ce13c91f6d";
+const emojiName = document.querySelector(".emoji-name");
+const emojiBox = document.querySelector(".emoji-box");
 const getBtn = document.querySelector("#get-btn");
 const copyBtn = document.querySelector("#copy-btn");
-const emojiBox = document.querySelector(".emoji-box");
-const emojiName = document.querySelector(".emoji-name");
-// Get emojis from api and store it into local storage
-const storeData = async () => {
+const historyWrapper = document.querySelector(".history-wrapper");
+let randNum;
+let emoji = localStorage.getItem("emoji");
+emoji = JSON.parse(emoji) || false;
+let history = [];
+
+const storeEmoji = async () => {
     try {
-        const res = await fetch(url);
-        const data = await res.json();
-        const strData = JSON.stringify(data);
-        localStorage.setItem("emojis", strData);
+        let res = await fetch(url);
+        let data = await res.json();
+        localStorage.setItem("emoji", JSON.stringify(data));
+        emoji = localStorage.getItem("emoji");
+        emoji = JSON.parse(emoji);
         getBtn.disabled = false;
-        showEmoji();
+        emojiName.innerText = "Now Go On! Get Emoji!";
+        emojiBox.innerText = "😈";
     } catch {
-        alert("Please Refresh the Page.");
+        emojiName.innerText = "SOMETHING WENT WRONG! PLEASE REFRESH THE PAGE.";
     }
 };
 
-const checkEmoji = () => {
-    if (localStorage.getItem("emojis")) {
-        getBtn.disabled = false;
-        showEmoji();
-        return true;
+document.addEventListener("DOMContentLoaded", () => {
+    if (!emoji) {
+        storeEmoji();
     } else {
-        storeData();
+        getBtn.disabled = false;
+        emojiName.innerText = "NOW GO ON! GET EMOJI!";
+        emojiBox.innerText = "😈";
+        showHistory();
     }
+});
+
+getBtn.addEventListener("click", () => {
+    randNum = Math.floor(Math.random() * 1859);
+    if (emoji) {
+        emojiBox.innerText = emoji[randNum].character;
+        emojiName.innerText = emoji[randNum].unicodeName.toUpperCase();
+    } else {
+        emojiName.innerText = "SORRY BUT, PLEASE REFRESH THE PAGE";
+        emojiBox.innerText = "😓";
+    }
+});
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(emojiBox.innerText);
+    if (localStorage.getItem("history")) {
+        let temp = localStorage.getItem("history");
+        temp = JSON.parse(temp);
+        let temp1 = emoji[randNum];
+        history = [...temp, temp1];
+        history = new Set(history);
+        history = [...history];
+        history = JSON.stringify(history);
+        localStorage.setItem("history", history);
+    } else {
+        history.push(emoji[randNum]);
+        history = JSON.stringify(history);
+        localStorage.setItem("history", history);
+    }
+});
+
+const showEmojiList = (emoji, name) => {
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    let span1 = document.createElement("span");
+    let btn = document.createElement("button");
+    div.classList.add("entry-con");
+    span.classList.add("copied-emoji");
+    span1.classList.add("copied-name");
+    span.innerText = emoji;
+    span1.innerText = name;
+    div.appendChild(span);
+    div.appendChild(span1);
+    div.appendChild(btn);
+    historyWrapper.appendChild(div);
 };
 
-const showEmoji = () => {
-    let num = Math.floor(Math.random() * 1859);
-    let emoji = localStorage.getItem("emojis");
-    emoji = JSON.parse(emoji);
-    emojiBox.innerText = emoji[num].character;
-    emojiName.innerText = emoji[num].unicodeName.toUpperCase();
+const showHistory = () => {
+    let historyEmoji = localStorage.getItem("history");
+    historyEmoji = JSON.parse(historyEmoji);
+    console.log(historyEmoji);
+    historyEmoji.forEach((ele) => {
+      if(ele){
+      showEmojiList(ele.character, ele.unicodeName)
+      }
+      else {
+        historyEmoji.pop(ele)
+      }
+    })
 };
-document.addEventListener("DOMContentLoaded", checkEmoji);
-getBtn.addEventListener("click", showEmoji);
-copyBtn.addEventListener("click", ()=>{
-  navigator.clipboard.writeText(emojiBox.innerText)
-});
